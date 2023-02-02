eb5affe09a81c2e2627974bab8fbdbc10c0568cd
pzajas <zajas.piotr@gmail.com>
2023-02-02 22:44:03 +0100

    #9: Create prisma seed functionality.

--- prisma/typescript/userTypes.ts
+++ prisma/typescript/userTypes.ts
@@ -1,9 +1,21 @@
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-interface User {
+export interface User {
   id: number
   username: string
   password: string
   email: string
   todos: Todo[]
+}
+
+export interface Seed {
+  username: string
+  email: string
+  password: string
+  todos: {
+    create: {
+      value: string
+      status: string
+    }
+  }
 }
