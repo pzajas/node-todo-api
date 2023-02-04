9f277777267f21fc5333e8538afccc69280757ad
pzajas <zajas.piotr@gmail.com>
2023-02-04 23:59:03 +0100

    #15: Create routers and services for user.

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import express from 'express'
 import { loginRouter } from './routes/auth/loginRouter'
 import { logoutRouter } from './routes/auth/logoutRouter'
 import { registerRouter } from './routes/auth/registerRouter'
+import { userRouter } from './routes/user/userRouter'
 
 const app = express()
 
@@ -13,5 +14,7 @@ app.get('/', (req: any, res: any) => {
 app.use('/', loginRouter)
 app.use('/', logoutRouter)
 app.use('/', registerRouter)
+
+app.use('/users', userRouter)
 
 app.listen(3000, () => 'server runs on 3000')
