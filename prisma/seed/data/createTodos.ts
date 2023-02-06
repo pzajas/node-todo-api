52009ced010a8d266524577ba13bd10c69cdf98c
pzajas <zajas.piotr@gmail.com>
2023-02-06 22:32:30 +0100

    #19: Change the folder names in the seed folder.

--- prisma/seed/data/createTodos.ts
+++ prisma/seed/data/createTodos.ts
@@ -1,6 +1,6 @@
 import { times } from 'lodash'
 
 import { type Todo } from '../../typescript/todoTypes'
-import { createRandomTodo } from '../data/mockTodo'
+import { createRandomTodo } from '../mock/mockTodo'
 
 export const createTodos = (count = 10): Todo[] => times(count, id => createRandomTodo(id))
--- prisma/seed/data/createUsers.ts
+++ prisma/seed/data/createUsers.ts
@@ -1,6 +1,6 @@
 import { times } from 'lodash'
 
 import { type User } from '../../typescript/userTypes'
-import { createRandomUser } from '../data/mockUser'
+import { createRandomUser } from '../mock/mockUser'
 
 export const createUsers = (count = 10): User[] => times(count, id => createRandomUser(id))
