5ab307543cc707eea8b411763cfb86992b39fad6
pzajas <zajas.piotr@gmail.com>
2023-02-10 23:24:11 +0100

    #22: Create initial controllers.

--- src/routes/auth/registerRouter.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-
-export const registerRouter = express.Router()
-
-registerRouter.route('/register').get((req: any, res: any) => {
-  res.render('register.ejs')
-})
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,9 @@
 import express from 'express'
 
-import { loginRouter } from './routes/auth/loginRouter'
-import { logoutRouter } from './routes/auth/logoutRouter'
-import { registerRouter } from './routes/auth/registerRouter'
-import { userRouter } from './routes/user/userRouter'
+import rootRouter from './routes'
 
 const app = express()
 
-app.get('/', (req: any, res: any) => {
-  res.render('index.ejs')
-})
+app.use('/', rootRouter)
 
-app.use('/', loginRouter)
-app.use('/', logoutRouter)
-app.use('/', registerRouter)
-
-app.use('/users', userRouter)
-
-app.listen(3000, () => 'server runs on 3000')
+app.listen(4000, () => 'server runs on 3000')
