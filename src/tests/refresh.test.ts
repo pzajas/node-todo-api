982426da693b30a50eb652cd3cc465e64cfde443
pzajas <zajas.piotr@gmail.com>
2023-02-20 23:05:46 +0100

    #37: Refactor tests code. Add more objects for response types.

--- src/tests/refresh.test.ts
+++ src/tests/refresh.test.ts
@@ -4,25 +4,19 @@ import dotenv from 'dotenv'
 import { env } from 'process'
 
 import { HTTP_CODES, HTTP_MESSAGES, HTTP_METHODS, HTTP_URLS } from '../interfaces/Responses/HTTP'
+import { signIn } from '../services/auth/signIn'
 
 dotenv.config()
 
 const username = env.LOGIN
 const password = env.PASSWORD
 
-let refreshToken: string = ''
+let refreshToken: string
 
 describe('user refreshes the token successfully', () => {
   beforeEach(async () => {
-    const response = await axios({
-      method: HTTP_METHODS.POST,
-      url: HTTP_URLS.LOGIN,
-      data: {
-        username,
-        password
-      }
-    })
-    refreshToken = response.data.refreshToken
+    const response = await signIn(username, password)
+    refreshToken = response.refreshToken
   })
 
   it('should provide a new access token to a logged user', async () => {
@@ -41,29 +35,24 @@ describe('user refreshes the token successfully', () => {
 
 describe('user tries to get a new token providing an invalid refresh token', () => {
   beforeEach(async () => {
-    const response = await axios({
-      method: HTTP_METHODS.POST,
-      url: HTTP_URLS.LOGIN,
-      data: {
-        username,
-        password
-      }
-    })
-    refreshToken = response.data.refreshToken
+    const response = await signIn(username, password)
+    refreshToken = response.refreshToken
   })
 
   it('should expect an error if an invalid refresh token is provided', async () => {
+    const invalidToken = `${refreshToken}random`
+
     await axios({
       method: HTTP_METHODS.POST,
       url: HTTP_URLS.REFRESH,
       data: {
-        refreshToken: `${refreshToken}random`
+        refreshToken: invalidToken
       }
 
     }).catch(err => {
       const response = err.response.data
 
-      if (refreshToken === `${refreshToken}random`) {
+      if (invalidToken !== null) {
         expect(response.status).eq(HTTP_CODES.UNAUTHORIZED)
         expect(response.message).eq(HTTP_MESSAGES.UNAUTHORIZED)
       }
@@ -71,17 +60,19 @@ describe('user tries to get a new token providing an invalid refresh token', () 
   })
 
   it('should expect an error if an empty refresh token is provided', async () => {
+    const invalidToken = ''
+
     await axios({
       method: HTTP_METHODS.POST,
       url: HTTP_URLS.REFRESH,
       data: {
-        refreshToken: ''
+        refreshToken: invalidToken
       }
 
     }).catch(err => {
       const response = err.response.data
 
-      if (refreshToken === '') {
+      if (invalidToken !== null) {
         expect(response.status).eq(HTTP_CODES.UNAUTHORIZED)
         expect(response.message).eq(HTTP_MESSAGES.UNAUTHORIZED)
       }
@@ -89,13 +80,16 @@ describe('user tries to get a new token providing an invalid refresh token', () 
   })
 
   it('should expect an error if no refresh token is provided', async () => {
+    let refreshToken: string
+
     await axios({
       method: HTTP_METHODS.POST,
       url: HTTP_URLS.REFRESH
+
     }).catch(err => {
       const response = err.response.data
 
-      if (refreshToken === null) {
+      if (refreshToken !== null) {
         expect(response.status).eq(HTTP_CODES.UNAUTHORIZED)
         expect(response.message).eq(HTTP_MESSAGES.UNAUTHORIZED)
       }
